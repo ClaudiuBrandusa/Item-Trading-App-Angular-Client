eb6b4ca615e8dc720f645a85dc42357ccd425605
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-10-23 15:22:42 +0300

    Remove authentication guard example from the identity routing module

--- src/app/modules/identity/identity-routing.module.ts
+++ src/app/modules/identity/identity-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthguardService as AuthGuardService } from 'src/app/guards/authguard.service';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent, canActivate: [AuthGuardService] }
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({

db9d15fd7ad573a0f4df48e4e6318b0cf3a85351
Claudiu Brandusa <claudiu.office99@gmail.com>
2023-11-05 21:01:45 +0200

    Improve default dialog

--- src/modules/trades/components/trade/trade.component.ts
+++ src/modules/trades/components/trade/trade.component.ts
@@ -34,8 +34,8 @@ export class TradeComponent implements OnInit {
   userId: string;
   respondedButtonClass: string;
   respondedIcon: string;
-  private baseRespondedButtonClass = "d-flex p-2 color-white no-select cursor-pointer";
-  private baseRespondedIconClass = " my-auto ms-1";
+  private baseRespondedButtonClass = "d-flex p-2 w-7-5rem color-white no-select cursor-pointer";
+  private baseRespondedIconClass = " my-auto ms-auto";
 
   constructor(private navigationService: NavigationService, private store: Store<Trade>, private tradeItemStore: Store<TradeItem>) {}
   
